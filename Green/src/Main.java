54d11f0c84f75931a08cdb552c1771a50bf447a9
Aaron Tran <100805011+aaront99@users.noreply.github.com>
2023-01-10 19:24:26 -0800

    Update Main.java

--- Green/src/Main.java
+++ Green/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int value = 10;
+        int value = 12;
         System.out.println(value + 5); //random number
         System.out.println("Hi team");
         System.out.println("Test");
