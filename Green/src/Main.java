f2df2f315ebe919cf846287bc3ad4939cfe45aa0
syivan <ivansyrups@gmail.com>
2023-01-10 19:13:25 -0800

    hello

---
+++ Green/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int value = 10;
+        System.out.println(value + 5); //random number
+        System.out.println("Hi team");
+    }
+}
